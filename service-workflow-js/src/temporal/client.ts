53a6637baaeaf02cb79627406ca8d58466110d4d
Ayrat Mustaev <aimustaev@avito.ru>
2025-05-13 19:14:03 +0300

    feat: удалил workflow-js

--- service-workflow-js/src/temporal/client.ts
+++
@@ -1,9 +0,0 @@
-import { Client, Connection } from "@temporalio/client";
-
-export const createClient = async (): Promise<Client> => {
-  const connection = await Connection.connect({
-    address: process.env.TEMPORAL_ADDRESS ?? "localhost:7233",
-  });
-
-  return new Client({ connection });
-}; 
